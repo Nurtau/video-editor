c0d8c075fbf951be9ae456d67529297b519c9560
Ayazhan <ayazhan.2318@gmail.com>
2023-11-07 22:04:09 +0600

    Chnaged scrollbar style

--- src/globalStyles.css.ts
+++ src/globalStyles.css.ts
@@ -10,3 +10,16 @@ globalStyle("html, body, #root", {
   width: "100%",
   height: "100%",
 });
+
+globalStyle("::-webkit-scrollbar", {
+  width: "5px",
+  height: "3px",
+});
+
+globalStyle("::-webkit-scrollbar-thumb", {
+  background: "rgba(90, 90, 90)",
+});
+
+globalStyle("::-webkit-scrollbar-track", {
+  background: "rgba(0, 0, 0, 0.2)",
+});
