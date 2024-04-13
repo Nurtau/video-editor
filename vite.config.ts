f9c3a18a45119711dceadfb5e4fa74bed4bd9c32
Nurtau <nurtaut@gmail.com>
2024-04-13 22:00:55 +0500

    Add vite-pwa to enable offline mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react-swc";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vanillaExtractPlugin(), react()],
+  plugins: [vanillaExtractPlugin(), react(), VitePWA()],
   resolve: {
     alias: {
       "~": fileURLToPath(new URL("./src", import.meta.url)),
